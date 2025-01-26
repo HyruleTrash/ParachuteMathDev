85b68ffabd000aa8a060646c451059ebc8cc83c8
HyruleTrash <83176601+HyruleTrash@users.noreply.github.com>
2025-01-26 12:52:36 +0100

    Start of force tracking, removing it tho

--- src/Parachute/Object/Body/RigidBody/RigidBody.cpp
+++ src/Parachute/Object/Body/RigidBody/RigidBody.cpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 #include <iostream>
 #include <cmath>
 #include <SFML/Graphics.hpp>
@@ -10,13 +11,77 @@
 
 using namespace Parachute;
 
+Force::Force(Force *other)
+{
+    this->force = other->force;
+    this->other = other->other;
+}
+
+Force::Force(Vector2 force, Body *other)
+{
+    this->force = force;
+    this->other = other;
+}
+
+Vector2 Force::GetSum(std::vector<Force> &forces)
+{
+    Vector2 result{};
+    for (auto forceData : forces)
+        result += forceData.force;
+    return result;
+}
+
+bool Force::Contains(std::vector<Force> &forces, Vector2 force, Body *other)
+{
+    for (auto forceData : forces)
+    {
+        if (forceData.force == force && forceData.other == other)
+            return true;
+    }
+
+    return false;
+}
+
+void Force::RemoveNegated(std::vector<Force> &forces)
+{
+    std::vector<Force> toRemove{};
+    // for (size_t i = 0; i < forces.size(); i++)
+    // {
+    //     Vector2 negationVector{-forces[i].force};
+    //     for (size_t j = 0; j < forces.size(); j++)
+    //     {
+    //         if (-forces[j].force == negationVector)
+    //         {
+    //             toRemove.push_back(Force{forces[j]});
+    //             toRemove.push_back(Force{forces[i]});
+    //         }
+    //     }
+    // }
+
+    for (auto force : toRemove)
+    {
+        for (int i = forces.size() - 1; i >= 0; i--)
+        {
+            if (forces[i].force == force.force && forces[i].other == force.other)
+            {
+                forces.erase(forces.begin() + i);
+                break;
+            }
+        }
+    }
+}
+
 void RigidBody::Update()
 {
-    // if it has a mass, and is supposed to be moving
-    if (mass != 0 && ((forces.GetMagnitude() != 0 || impulses.GetMagnitude() != 0) || velocity != V2_ZERO))
-    {
-        Vector2 totalForce{forces};
+    // prepare forces
+    Force::RemoveNegated(forces);
+    Force::RemoveNegated(impulses);
+    Vector2 totalForce{Force::GetSum(forces)};
+    Vector2 totalImpulses{Force::GetSum(impulses)};
 
+    // if it has a mass, and is supposed to be moving
+    if (mass != 0 && ((totalForce.GetMagnitude() != 0 || totalImpulses.GetMagnitude() != 0) || velocity != V2_ZERO))
+    {
         // add drag if body has velocity
         if (velocity.GetMagnitude() != 0)
         {
@@ -25,7 +90,7 @@ void RigidBody::Update()
 
         Vector2 nextVelocity{MathUtil::Physics::GetNextVelocity(velocity, totalForce, mass, game->time.deltaTime)};
 
-        nextVelocity += impulses;
+        nextVelocity += totalImpulses;
 
         // Limit Velocity, for terminal velocity
         const double maxVelocityMagnitude{MathUtil::GetMagnitude(MAX_VELOCITY)};
@@ -41,7 +106,7 @@ void RigidBody::Update()
         velocity = nextVelocity;
     }
 
-    if (forces.GetMagnitude() == 0 && impulses.GetMagnitude() == 0)
+    if (totalForce.GetMagnitude() == 0 && totalImpulses.GetMagnitude() == 0)
     {
         if (abs(velocity.x) < 1)
             velocity.x = 0;
@@ -54,23 +119,35 @@ void RigidBody::Update()
 
 void RigidBody::AddForce(Vector2 force)
 {
-    forces += force;
+    this->AddForce(force, this);
 }
 
 void RigidBody::AddImpulse(Vector2 impulse)
 {
-    impulses += impulse;
+    this->AddImpulse(impulse, this);
+}
+
+void RigidBody::AddForce(Vector2 force, Body *other)
+{
+    if (!Force::Contains(forces, force, other))
+        forces.push_back(Force{force, other});
+}
+
+void RigidBody::AddImpulse(Vector2 impulse, Body *other)
+{
+    if (!Force::Contains(impulses, impulse, other))
+        impulses.push_back(Force{impulse, other});
 }
 
 void RigidBody::OnCollided(Body *other, Vector2 collisionNormal)
 {
     double density = other->density;
-    AddImpulse(collisionNormal * density);
+    AddImpulse(collisionNormal * density, other);
 }
 
 void RigidBody::OnCollisionEnded(Body *other, IntersectionData data)
 {
     Body *oldData = dynamic_cast<Body *>(data.intersectorOldData);
     double density = oldData->density;
-    AddImpulse(-data.appliedNormalVector * density);
+    AddImpulse(-data.appliedNormalVector * density, other);
 }
--- src/Parachute/Object/Body/RigidBody/RigidBody.h
+++ src/Parachute/Object/Body/RigidBody/RigidBody.h
@@ -3,12 +3,26 @@
 
 namespace Parachute
 {
+    class Force
+    {
+    public:
+        Force() = default;
+        Force(Force *);
+        Force(Vector2 force, Body *other);
+        ~Force() = default;
+        static Vector2 GetSum(std::vector<Force> &);
+        static bool Contains(std::vector<Force> &, Vector2 force, Body *other);
+        static void RemoveNegated(std::vector<Force> &);
+        Vector2 force{};
+        Body *other{};
+    };
+
     class RigidBody : public Body
     {
     private:
         constexpr static Vector2 MAX_VELOCITY{200, 200};
-        Vector2 forces{V2_ZERO};
-        Vector2 impulses{V2_ZERO};
+        std::vector<Force> forces{};
+        std::vector<Force> impulses{};
 
     public:
         RigidBody() = default;
@@ -16,6 +30,8 @@ namespace Parachute
         void Update() override;
         void AddForce(Vector2 force);
         void AddImpulse(Vector2 impulse);
+        void AddForce(Vector2 force, Body *other);
+        void AddImpulse(Vector2 impulse, Body *other);
         void OnCollided(Body *other, Vector2 collisionNormal);
         void OnCollisionEnded(Body *other, IntersectionData);
         Vector2 velocity{V2_ZERO};
